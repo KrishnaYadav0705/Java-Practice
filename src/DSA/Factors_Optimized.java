ec30f63b947b5f47c92af213db19dacbdc603e21
Krishna Yadav <mayay2714@gmail.com>
2026-03-24 22:10:47 +0530

    Added optimized approach for finding factors.

---
+++ src/DSA/Factors_Optimized.java
@@ -0,0 +1,25 @@
+package DSA;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Factors_Optimized {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n = sc.nextInt();
+        ArrayList<Integer> factors = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if(n%i == 0 && i*i != n){
+                factors.add(i);
+                factors.add(n/i);
+            }
+//            if(i*i != n && n%i == 0){
+//                factors.add(n/i);
+//            }
+        }
+        Collections.sort(factors);
+        System.out.println(factors);
+    }
+}
